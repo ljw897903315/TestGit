b636fd67da7c873c1b0d23e4d551ab30a0d1fc61
ljw897903315 <897903315@qq.com>
2019-11-14 11:27:21 +0800

    first commit

---
+++ src/cn/jc/demo/Demo1.java
@@ -0,0 +1,29 @@
+package cn.jc.demo;
+
+import java.util.ArrayList;
+
+public class Demo1 {
+    //注释的快捷键ctrl+/
+    //多行注释ctrl+shift+/
+    //快速删除ctrl+Y
+    //快速复制ctrl+D
+    //快速开始新的一行shift+enter
+    //上下移动ctrl+shift+↑↓
+    public static void main(String[] args) {
+
+        System.out.println("helloworld");
+        //先写再考虑
+        System.out.println("你好，java!");
+        if (5 < 4) {
+            System.out.println("false");
+        }
+        //快速生成变量ctrl+alt+V
+        ArrayList<String> list = new ArrayList<>();
+        for (String s : list) {
+
+        }
+        //alt+enter可以跳出除了类包联想外的解决方式
+        //双shift{快速按两下）全局搜索
+       
+    }
+}
